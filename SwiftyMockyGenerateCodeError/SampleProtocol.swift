fec0acacb290cca2df07cc1f437f29717d852581
ji-no <jino.jpn@gmail.com>
2023-10-19 02:37:24 +0900

    Create AutoMockable protocol

---
+++ SwiftyMockyGenerateCodeError/SampleProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  SampleProtocol.swift
+//  SwiftyMockyGenerateCodeError
+//  
+//  Created by ji-no on 2023/10/19
+//  
+//
+
+import Foundation
+
+struct Sample {
+}
+
+// sourcery: AutoMockable
+protocol SampleProtocol {
+
+    func success() -> Sample
+    
+}
